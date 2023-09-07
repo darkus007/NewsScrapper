36f31fceb26ee57954151e0f48a040ff6a5c6bc0
Sergey <darkus007@yandex.ru>
2023-09-07 21:13:05 +0300

    Создан шаблон проекта.

---
+++ spider.py
@@ -0,0 +1,8 @@
+"""
+spider.py load http://www.vesti.ru/ --depth 2
+>> ok, execution time: 10s, peak memory usage: 100 Mb
+spider.py get http://www.vesti.ru/ -n 2
+>> http://www.vesti.ru/news/: "Вести.Ru: новости, видео и фото дня"
+>> http://www.vestifinance.ru/: "Вести Экономика: Главные события российской и мировой экономики, деловые новости,  фондовый рынок"
+
+"""
